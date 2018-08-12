fd3b0afeb0e5663b814b083f5e371a1efe86cb0d
Jan-shin <Jan-shin@gogomail.com>
2018-08-12 16:04:08 +0800

    [Add] Set parameter to linux divece driver module

---
+++ module_parameter/.tmp_versions/para.mod
@@ -0,0 +1,2 @@
+/root/raspberry/driver_module/module_parameter/para.ko
+/root/raspberry/driver_module/module_parameter/para.o
---
+++ module_parameter/para.c
@@ -0,0 +1,29 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static char *book_name = "baohua Linux Device Driver";
+module_param(book_name, charp, S_IRUGO);
+
+static int book_num = 4000;
+/* kernel won't create node on /sys/modules/para/parameter for this parameter'*/
+module_param(book_num, int, 0);
+
+
+
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "book name: %s\n", book_name);
+	printk(KERN_INFO "book num: %d\n", book_num);
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	printk(KERN_ALERT "driver unloaded\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
